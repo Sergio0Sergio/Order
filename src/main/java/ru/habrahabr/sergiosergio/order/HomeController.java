20a5aca3fc57c094ef856f1b913a5f3287711be1
Sergei Grimanov <tosergi@gmail.com>
2017-09-06 16:36:15 +0300

    first commit

---
+++ src/main/java/ru/habrahabr/sergiosergio/order/HomeController.java
@@ -0,0 +1,13 @@
+package ru.habrahabr.sergiosergio.order;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RestController
+public class HomeController {
+
+   @RequestMapping("/")
+    public String index(){
+       return "тест";
+
+    }
+}
