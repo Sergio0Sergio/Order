1ac3420970b5e85f47c49299cf7be82d8b21e962
Sergei Grimanov <tosergi@gmail.com>
2017-09-10 23:13:16 +0300

    greeting page and controller has been finished

---
+++ src/main/java/ru/habrahabr/sergiosergio/order/Greeting.java
@@ -0,0 +1,21 @@
+package ru.habrahabr.sergiosergio.order;
+
+public class Greeting {
+
+    private int inn;
+    private int captcha;
+
+    public int getInn() {return inn;  }
+
+    public void setInn(int inn) {
+        this.inn = inn;
+    }
+
+    public int getCaptcha() {
+        return captcha;
+    }
+
+    public void setCaptcha(int captchacha) {
+        this.captcha = captcha;
+    }
+}
--- src/main/java/ru/habrahabr/sergiosergio/order/HomeController.java
+++ src/main/java/ru/habrahabr/sergiosergio/order/HomeController.java
@@ -1,13 +1,51 @@
 package ru.habrahabr.sergiosergio.order;
 
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-@RestController
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+
+@Controller
 public class HomeController {
+    String imageSourse;
+    private Greeting greeting;
+    Document webPage;
 
-   @RequestMapping("/")
-    public String index(){
-       return "тест";
+    @GetMapping("/greeting")
+    public String greetingForm(Model model){
+        model.addAttribute("greeting", new Greeting());
 
+        try {
+            webPage = Jsoup.connect("https://egrul.nalog.ru/").get();
+        } catch (IOException e) {
+            System.err.println("страница ФНС недоступна.");
+            e.printStackTrace();
+        }
+        Element image = webPage.select("img").get(0);
+        imageSourse = image.absUrl("src");
+        model.addAttribute("imageSource", imageSourse );
+        return "greeting";
+    }
+    @PostMapping(value = "/greeting")
+    public String greetingSubmit(@ModelAttribute Greeting greeting){
+        this.greeting = greeting;
+
+        try {
+            webPage = Jsoup.connect("https://egrul.nalog.ru/").get();
+        } catch (IOException e) {
+            System.err.println("страница ФНС недоступна.");
+            e.printStackTrace();
+        }
+
+        Element table = webPage.select("table").get(0);
+        Elements row = table.select("tr");
+
+
+        return "result";
     }
 }
